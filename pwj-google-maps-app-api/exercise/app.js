cba5930b40f0fecb97d501b30adf1d276d6a16b6
gary <gary@ligginsdevelopment.com>
2021-10-05 16:47:22 -0700

    connecting mongoDB

--- pwj-google-maps-app-api/exercise/app.js
+++ pwj-google-maps-app-api/exercise/app.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://gary_liggins:Alysha5545@cluster0.fxf9m.mongodb.net/googleMaps?retryWrites=true&w=majority', {
+   useNewUrlParser : true, useUnifiedTopology: true 
+});
+
+app.use(express.json({limit: "50mb"}));
+
+app.post("/api/stores", (req,res) => {
+    let dbStores = req.body
+    console.log(dbStores);
+    res.send("you have posted")
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
