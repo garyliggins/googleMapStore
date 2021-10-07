420c1f248412ab7aacaaecdb4c746c33b3e1683f
gary <gary@ligginsdevelopment.com>
2021-10-06 17:11:07 -0700

    completed server, working on map functions

--- frontend/index.js
+++ frontend/index.js
@@ -5,12 +5,44 @@ function initMap() {
       center: losAngeles,
       zoom: 8
     });
-    createMarker();
+    getStores();
+    
 }
 
-const createMarker = () => {
+
+
+const searchLocationsNear = (stores) => {
+    let bounds = new google.maps.LatLngBounds();
+    stores.forEach((store,index) => {
+        let latlng = new google.maps.LatLng(
+            store.location.coordinates[1],
+            store.location.coordinates[0]);
+            let name = store.storeName;
+            let address = store.addressLines[0];
+            bounds.extend(latlng);
+            createMarker(latlng, name, address)
+    })
+    map.fitBounds(bounds)
+}
+
+const createMarker = (latlng, name, address) => {
     var marker = new google.maps.Marker({
-        position: {lat: 34.063380, lng: -118.358080},
+        position: latlng,
         map: map
     }) 
 }
+
+const getStores = () => {
+    const API_URL = 'http://localhost:3000/api/stores';
+    fetch(API_URL)
+    .then((response) =>{
+       if( response.status == 200){
+           return response.json();
+       } else {
+           throw new Error(response.status);
+       }
+    }).then((data) => {
+        searchLocationsNear(data);
+    })
+}
+
